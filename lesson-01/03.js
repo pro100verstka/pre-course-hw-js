76e91601bc7061b8eb7c8b11a48b22ed4dfbc424
pro100verstka <pro100verstka@gmail.com>
2023-12-05 22:09:10 +0500

    Задание 3

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,9 +1,5 @@
 let number = 1;
-const number1 = number++;
-const number2 = ++number;
-const number3 = number--;
-const number4 = --number;
-console.log(number1); 
-console.log(number2); 
-console.log(number3); 
-console.log(number4); 
+console.log(number++); 
+console.log(++number); 
+console.log(number--); 
+console.log(--number); 
