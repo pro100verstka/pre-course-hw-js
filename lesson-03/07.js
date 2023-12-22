a02cfd7aa51690acc0927ed24f2460922e10d65c
pro100verstka <pro100verstka@gmail.com>
2023-12-22 09:44:00 +0500

    Задание 03-07

---
+++ lesson-03/07.js
@@ -0,0 +1,12 @@
+const numbers = [
+    [1, 2, 3, 4, 5],
+    [11, 20, 33, 40, 55],
+    [111, 200, 333, 400, 555],
+];
+
+for (let i = 0; i < numbers.length; i++) {
+	numbers[i] = numbers[i].filter(function(num) {
+		return num % 2 === 0;
+	});
+}
+console.log(numbers);
