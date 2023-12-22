4feebafca06716d07404696f02afe33bbd2bf2af
pro100verstka <pro100verstka@gmail.com>
2023-12-22 07:50:01 +0500

    Задание 03-01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,7 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter((item) => item === "черный" || item === "красный" || item === "желтый").join("-");
+}
+
+console.log(createColorString());
